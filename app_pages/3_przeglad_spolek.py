672e1a9bbbe8a2ccfb1a7966944631ffde8f7c72
WaznyKamo <waznykamil@wp.pl>
2025-11-04 18:50:19 +0100

    added column autosize

--- app_pages/3_przeglad_spolek.py
+++ app_pages/3_przeglad_spolek.py
@@ -8,11 +8,17 @@ st.write("Narzędzie do identyfikacji spółek o wysokim potencjale wzrostu.")
 latest_data = st.session_state.latest_data
 latest_data = latest_data.sort_values(by='Ticker')
 
-# Konfiguracja tabeli AgGrid
 gb = GridOptionsBuilder.from_dataframe(latest_data)
-gb.configure_default_column(editable=True, groupable=True, filter=True, sortable=True)
-# gb.configure_pagination(paginationAutoPageSize=True)  # automatyczne strony
-gb.configure_side_bar()  # panel boczny z opcjami filtrowania/sortowania
+gb.configure_default_column(
+    editable=True,
+    groupable=True,
+    filter=True,
+    sortable=True,
+    minWidth=120,
+    wrapHeaderText=True,
+    autoHeaderHeight=True
+)
+gb.configure_side_bar()
 
 grid_options = gb.build()
 
@@ -21,7 +27,8 @@ AgGrid(
     gridOptions=grid_options,
     update_mode=GridUpdateMode.NO_UPDATE,
     enable_enterprise_modules=True,
-    theme="streamlit",  
+    theme="streamlit",
     height=600,
-    columns_auto_size_mode=ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW
-    )
+    columns_auto_size_mode=ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW,
+    fit_columns_on_grid_load=True
+)
